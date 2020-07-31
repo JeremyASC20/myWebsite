2971d82e43ee42ed9d60fb3a773fc57e151b5f68
unknown <lij061703@gmail.com>
2020-07-31 14:23:23 -0400

    tenth commit

---
+++ script.js
@@ -0,0 +1,103 @@
+let text1 = document.getElementById("bay_max");
+let image1 = document.getElementById("baymax");
+let text2 = document.getElementById("BT");
+let image2 = document.getElementById("bear");
+let text3 = document.getElementById("Island");
+let image3 = document.getElementById("governor");
+let text4 = document.getElementById("movieT");
+let image4 = document.getElementById("movie");
+let image5 = document.getElementById("movie2");
+let text6 = document.getElementById("curious");
+let image6 = document.getElementById("young");
+let text7 = document.getElementById("city");
+let image7 = document.getElementById("industry");
+let text8 = document.getElementById("ASC");
+let image8 = document.getElementById("asc");
+
+text1.style.display = "none";
+text2.style.display = "none";
+text3.style.display = "none";
+text4.style.display = "none";
+text6.style.display = "none";
+text7.style.display = "none";
+text8.style.display = "none";
+
+image1.onmouseover = function() {
+    text1.style.display = "block";
+    image1.style.opacity = "0.5";
+}
+
+image1.onmouseout = function() {
+    text1.style.display = "none";
+    image1.style.opacity = "1";
+}
+
+image2.onmouseover = function() {
+    text2.style.display = "block";
+    image2.style.opacity = "0.5";
+}
+
+image2.onmouseout = function() {
+    text2.style.display = "none";
+    image2.style.opacity = "1";
+}
+
+image3.onmouseover = function() {
+    text3.style.display = "block";
+    image3.style.opacity = "0.5";
+}
+
+image3.onmouseout = function() {
+    text3.style.display = "none";
+    image3.style.opacity = "1";
+}
+
+image4.onmouseover = function() {
+    text4.style.display = "block";
+    image4.style.opacity = "0.5";
+}
+
+image4.onmouseout = function() {
+    text4.style.display = "none";
+    image4.style.opacity = "1";
+}
+
+image5.onmouseover = function() {
+    text4.style.display = "block";
+    image5.style.opacity = "0.5";
+}
+
+image5.onmouseout = function() {
+    text4.style.display = "none";
+    image5.style.opacity = "1";
+}
+
+image6.onmouseover = function() {
+    text6.style.display = "block";
+    image6.style.opacity = "0.5";
+}
+
+image6.onmouseout = function() {
+    text6.style.display = "none";
+    image6.style.opacity = "1";
+}
+
+image7.onmouseover = function() {
+    text7.style.display = "block";
+    image7.style.opacity = "0.5";
+}
+
+image7.onmouseout = function() {
+    text7.style.display = "none";
+    image7.style.opacity = "1";
+}
+
+image8.onmouseover = function() {
+    text8.style.display = "block";
+    image8.style.opacity = "0.5";
+}
+
+image8.onmouseout = function() {
+    text8.style.display = "none";
+    image8.style.opacity = "1";
+}
